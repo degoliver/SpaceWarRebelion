2542ae3ec936fe83b9ef105d8898b3b23fd8bc6f
Usuário Convidado <Guest@l2304micro22.local>
2016-03-02 22:25:04 -0300

    Turbinas e shield.

--- SpaceWarRebellion/Classes/TSTUtils/Constantes.swift
+++ SpaceWarRebellion/Classes/TSTUtils/Constantes.swift
@@ -10,9 +10,10 @@ let screenSize:CGSize = CCDirector.sharedDirector().viewSize()
 
 // Layers dos objetos
 enum ObjectsLayers:Int {
-	case HUD = 5
-	case Shot = 4
-	case Player = 3
+	case HUD = 6
+	case Shot = 5
+	case Player = 4
+    case turbina = 3
 	case Foes = 2
 	case Background = 1
 }
--- SpaceWarRebellion/Objects/PlayerShip.swift
+++ SpaceWarRebellion/Objects/PlayerShip.swift
@@ -8,12 +8,19 @@
 // MARK: - Class Definition
 class PlayerShip : CCSprite {
 	// MARK: - Public Objects
+    internal var eventSelector:Selector?
+    internal var targetID:AnyObject?
+    private var alive:Bool = true
+    
 	var atackSpeed:CGFloat = 5.0
 	var life:CGFloat = 100.0
 
-	// MARK: - Private Objects
-	
-	// MARK: - Life Cycle
+    var shieldLife:CGFloat = 0.0
+    var shieldDuration:CGFloat = 0.0
+    var shieldCount:CGFloat = 3.0
+    
+    private var shield:CCSprite = CCSprite(imageNamed: "heroShield.png")
+    
 	override init() {
 		super.init()
 	}
@@ -50,12 +57,57 @@ class PlayerShip : CCSprite {
 		self.physicsBody.collisionType = "PlayerShip"
 		self.physicsBody.collisionCategories = ["PlayerShip"]
 		self.physicsBody.collisionMask = ["EnemyShip", "EnemyShot", "Asteroid"]
+        
+        criaTurbinas()
+        
+        shield.scale = 0.0
+        self.shield.anchorPoint = CGPointMake(0.5, 0.5)
+        self.shield.position = CGPointMake(self.contentSize.width/2, self.contentSize.height/2)
+        self.addChild(shield, z: ObjectsLayers.Shot.rawValue)
 	}
 	
 	override func onEnter() {
 		// Chamado apos o init quando entra no director
 		super.onEnter()
 	}
+    
+    func activeShield(){
+        if(self.shieldCount >= 1){
+            shieldCount--
+            self.shieldLife = 100.0
+            self.shield.runAction(CCActionScaleTo.actionWithDuration(0.2, scale: 1.5) as! CCAction) as CCAction
+            DelayHelper.sharedInstance.callBlock({ () -> Void in
+                self.finishShield()
+                }, withDelay: 20.0)
+        }
+    }
+    
+    func finishShield(){
+        self.shieldLife = 0.0
+        self.shield.scale = 0.0
+    }
+    
+    //função responsável pela geração das turbinas.
+    func criaTurbinas(){
+        let rightBigParticle:CCParticleSystem = CCParticleSystem(file: "turbina1.plist")
+        rightBigParticle.position = CGPoint(x: (self.contentSize.width/2) + 16.0, y: self.contentSize.width - 145.0)
+        self.addChild(rightBigParticle, z:ObjectsLayers.turbina.rawValue)
+        
+        let leftBigParticle:CCParticleSystem = CCParticleSystem(file: "turbina1.plist")
+        leftBigParticle.position = CGPoint(x: (self.contentSize.width/2) - 16.0, y: self.contentSize.width - 145.0)
+        self.addChild(leftBigParticle, z:ObjectsLayers.turbina.rawValue)
+        
+        let rightLittleParticle:CCParticleSystem = CCParticleSystem(file: "turbina1.plist")
+        rightLittleParticle.position = CGPoint(x: (self.contentSize.width/2) + 40.0, y: self.contentSize.width - 132.0)
+        rightLittleParticle.scale = 0.5
+        self.addChild(rightLittleParticle, z:ObjectsLayers.turbina.rawValue)
+        
+        let leftLittleParticle:CCParticleSystem = CCParticleSystem(file: "turbina1.plist")
+        leftLittleParticle.position = CGPoint(x: (self.contentSize.width/2) - 40.0, y: self.contentSize.width - 132.0)
+        leftLittleParticle.scale = 0.5
+        self.addChild(leftLittleParticle, z:ObjectsLayers.turbina.rawValue)
+
+    }
 	
 	// MARK: - Private Methods
 	
